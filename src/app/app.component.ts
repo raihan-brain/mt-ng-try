6f4e91fee2799ea9084b5a1e2df4ebadced80d3b
raihan-zaman <raihanuzzaman@bs-23.net>
2023-05-03 18:10:25 +0600

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'mt-ng-try';
+
+  ngOnInit(): void {
+    console.log('===== working on it');
+  }
 }
